71abc77872af61fb1a7bbc68abae447b903e0ca1
noamsalo <noamsal30@gmail.com>
2025-11-07 15:16:52 +0200

    Fix index check for moving '1' in list

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -5,7 +5,7 @@ def move(my_list, direction):
 
     # Move the one to the left or to the right
     if direction == 'right':
-        if index_of_one != len(my_list): 
+        if index_of_one != len(my_list) - 1: 
             my_list[index_of_one] = 0
             my_list[index_of_one + 1] = 1
 
