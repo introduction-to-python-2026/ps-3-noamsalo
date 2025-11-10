adb1dbd1b3ca88f9187b167c1d392cec4f466fee
noamsalo <noamsal30@gmail.com>
2025-11-10 21:50:47 +0200

    Implement Leibniz series to approximate π

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,6 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    leibniz_series = []  
+    for i in range(n_terms):
+        term = ((-1) ** i) / (2 * i + 1)
+        leibniz_series.append(term)
+    return sum(leibniz_series) * 4
